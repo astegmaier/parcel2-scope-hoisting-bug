bb0a24f09b6d497ec8472b0fd3326794d042d0f1
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-03-30 10:50:59 -0400

    Get basic "real" repro.

--- parcel-repro-real/src/index.js
+++ parcel-repro-real/src/index.js
@@ -1,7 +1,11 @@
-// import { AnimationClassNames } from "@fluentui/style-utilities";
+// These do not:
 
+// import { AnimationClassNames } from "@fluentui/style-utilities";
 // console.log("here is AnimationClassNames: ", AnimationClassNames);
 
-import { buildClassMap } from "@fluentui/style-utilities";
+// import { buildClassMap } from "@fluentui/style-utilities";
+// console.log("here is buildClassMap: ", buildClassMap);
 
-console.log("here is buildClassMap: ", buildClassMap);
+// This triggers the issue:
+import { initializeIcons } from "@fluentui/react";
+initializeIcons();
