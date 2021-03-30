3f2edb5b5c83f860cceb3de0275d38b079ae3382
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-03-30 10:22:36 -0400

    scaffold "real" repro

--- parcel-repro-real/src/index.js
+++ parcel-repro-real/src/index.js
@@ -1,1 +1,7 @@
-console.log("hello!");
+// import { AnimationClassNames } from "@fluentui/style-utilities";
+
+// console.log("here is AnimationClassNames: ", AnimationClassNames);
+
+import { buildClassMap } from "@fluentui/style-utilities";
+
+console.log("here is buildClassMap: ", buildClassMap);
