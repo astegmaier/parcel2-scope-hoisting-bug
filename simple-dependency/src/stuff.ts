bf3bd61ab06de5a91181b7215d757c53eca00276
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-03-31 16:12:41 -0400

    Add clarifying comment.

--- simple-dependency/src/stuff.ts
+++ simple-dependency/src/stuff.ts
@@ -2,4 +2,5 @@ import { getGlobalClassNames } from "@fluentui/style-utilities";
 
 export const classNames = getGlobalClassNames({} as any, {} as any, false);
 
+// This is the only thing that we're ultimately going to try to import into parcel.
 export const aStringExport = "here is a string exported from simple-dependency";
