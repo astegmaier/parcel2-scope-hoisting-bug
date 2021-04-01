1c663e14bd7fb5f5e41cadb508a5604d6e0b5e71
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-03-31 21:17:41 -0400

    Cleanup.

--- simple-dependency2/src/index.ts
+++ simple-dependency2/src/index.ts
@@ -1,14 +1,3 @@
-// export * from './classNames/index';
 export * from "./styles/index";
-// export * from './utilities/index';
-// export * from './interfaces/index';
-// export * from './MergeStyles';
 
-// import './version';
-
-// eslint-disable-next-line no-console
 console.log("here is a side effect!");
-
-// Ensure theme is initialized when this package is referenced.
-// import { initializeThemeInCustomizations } from './styles/theme';
-// initializeThemeInCustomizations();
