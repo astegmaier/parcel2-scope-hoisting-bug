84c1456a214216f93606b542bb8ece067c056594
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-03-31 21:14:15 -0400

    Add baselines.

--- simple-dependency2/src/index.ts
+++ simple-dependency2/src/index.ts
@@ -1,5 +1,5 @@
 // export * from './classNames/index';
-export * from './styles/index';
+export * from "./styles/index";
 // export * from './utilities/index';
 // export * from './interfaces/index';
 // export * from './MergeStyles';
@@ -7,7 +7,7 @@ export * from './styles/index';
 // import './version';
 
 // eslint-disable-next-line no-console
-console.log('here is a side effect!');
+console.log("here is a side effect!");
 
 // Ensure theme is initialized when this package is referenced.
 // import { initializeThemeInCustomizations } from './styles/theme';
