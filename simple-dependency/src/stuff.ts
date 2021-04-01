1621f4d7eb28ca4bb1b5e156ef432c4aa2843dc6
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-03-31 21:25:55 -0400

    Cleanup getGlobalClassNames

--- simple-dependency/src/stuff.ts
+++ simple-dependency/src/stuff.ts
@@ -1,6 +1,6 @@
 import { getGlobalClassNames } from "simple-dependency2";
 
-export const classNames = getGlobalClassNames({} as any, {} as any, false);
+export const classNames = getGlobalClassNames();
 
 // This is the only thing that we're ultimately going to try to import into parcel.
 export const aStringExport = "here is a string exported from simple-dependency";
