033d4ab01f473ecf50c7ee2fed333042d147077d
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-03-31 21:45:49 -0400

    Better comments

--- simple-dependency/src/product.ts
+++ simple-dependency/src/product.ts
@@ -1,3 +1,4 @@
 import { multiply } from "simple-dependency2";
 
+// This is unused by parcel-repro-simple
 export const product = multiply(3, 3);
--- simple-dependency/src/sum.ts
+++ simple-dependency/src/sum.ts
@@ -1,6 +1,7 @@
 import { add } from "simple-dependency2";
 
-export const sum = add(3, 3); // unused by parcel
+// This is unused by parcel-repro-simple
+export const sum = add(3, 3);
 
 // This is the only thing that we're ultimately going to try to import into parcel.
 export const aStringExport = "here is a string exported from simple-dependency";
