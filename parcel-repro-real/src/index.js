aacaf522bb568b113dadbef7bd75208d16000615
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-04-01 10:18:03 -0400

    More cleanup.

--- parcel-repro-real/src/index.js
+++ parcel-repro-real/src/index.js
@@ -1,31 +1,7 @@
-// These do not:
+// Importing any component from @fluentui/react will trigger a runtime error when scope-hoisting is on.
+import { Overlay } from "@fluentui/react";
+console.log("here is Overlay: ", Overlay);
 
-// import { AnimationClassNames } from "@fluentui/style-utilities";
-// console.log("here is AnimationClassNames: ", AnimationClassNames);
-
-// import { buildClassMap } from "@fluentui/style-utilities";
-// console.log("here is buildClassMap: ", buildClassMap);
-
-// This triggers the issue:
-import { initializeIcons } from "@fluentui/react";
-initializeIcons();
-
-// This also triggers a different issue:
-// import { Overlay } from "@fluentui/react";
-// console.log("here is Overlay: ", Overlay);
-
-// "aString" was exported from "@fluentui/react/src/index.ts", and it works fine.
-// import { aString } from "@fluentui/react";
-// console.log(aString)
-
-// This works, too, but the and the output starts using $parcel$exportWildcard
-// import { aStringFromOverlay } from "@fluentui/react";
-// console.log(aStringFromOverlay);
-
-// This works, too.
-// import { aStringFromOverlayIndex } from "@fluentui/react";
-// console.log(aStringFromOverlayIndex);
-
-// This blows up (hooray!). It's imported from "@fluentui/react/src/components/Overlay/Overlay.ts"
-// import { aStringFromOverlayOverlay } from "@fluentui/react";
-// console.log(aStringFromOverlayOverlay);
+// This also triggers the issue:
+// import { initializeIcons } from "@fluentui/react";
+// initializeIcons();
